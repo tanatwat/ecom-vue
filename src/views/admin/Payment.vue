8eec94bcce13f8db44ecacdeaa8f1505e3e8813a
Tanatwat Wattanasakul <tanatwat.w@gmail.com>
2019-05-06 10:43:40 +0700

    finish payments page

--- src/views/admin/Payment.vue
+++ src/views/admin/Payment.vue
@@ -1,51 +1,165 @@
 <template>
-  <div>
-    <div class="page-title">
-      <h1 class="page-title-heading">Title</h1><a href="#" class="button secondary w-button">ย้อนกลับ</a></div>
-    <div class="w-row">
-      <div class="left-column w-col w-col-6">
-        <div class="box">
-          <h3 class="section-heading">ช่องทางชำระเงินของร้าน</h3>
-          <div class="data-list-wrapper w-clearfix">
-            <div>
-              <div class="data-list-heading">Category</div>
+  <div class="columns is-multiline">
+    <div class="column is-full">
+      <button class="btn primary" @click="$root.showModal = true">
+        เพิ่มช่องทางชำระเงิน&nbsp;
+        <i class="fas fa-plus"></i>
+      </button>
+    </div>
+
+    <modal>
+      <header slot="header" class="modal-card-head">
+        <p class="modal-card-title">เพิ่มช่องทางชำระเงิน</p>
+        <button class="btn primary fas fa-times" @click="$root.showModal = false" type="button"></button>
+      </header>
+      <form method="post" @submit.prevent="addPayment()" slot="body">
+        <section class="modal-card-body">
+          <div class="margin-center" style="max-width:350px">
+            <label class="label">ประเภทบัญชี</label>
+            <div class="field">
+              <div class="control">
+                <div class="select is-fullwidth">
+                  <select required v-model="form.method">
+                    <option value="account">บัญชีธนาคาร</option>
+                    <option value="PromptPay">PromptPay</option>
+                  </select>
+                </div>
+              </div>
             </div>
-            <div class="data-list-body">
-              <div class="data-list-text">Subcategory</div>
-              <div class="data-list-text">Subcategory</div>
+
+            <div class="field">
+              <label class="label">ธนาคาร</label>
+              <div class="control">
+                <div class="select is-fullwidth">
+                  <select required v-model="form.provider">
+                    <option :value="bank.name" v-for="bank in providers">{{ bank.name }}</option>
+                  </select>
+                </div>
+              </div>
             </div>
-            <div class="action-wrapper"><a href="#" class="button-icon edit w-button">Edit</a><a href="#" class="button-icon delete w-button">Delete</a></div>
+
+            <form-input
+              label="เลขบัญชี"
+              name="name"
+              type="text"
+              validate="required"
+              validate-as="เลขบัญชี"
+              v-model="form.ref"
+              placeholder="เช่น 123-456789-0"
+            ></form-input>
           </div>
-          <ul class="no-style-list"></ul>
+        </section>
+        <footer class="modal-card-foot action-wrapper right has-margin" slot="footer">
+          <button :disabled="errors.any()" class="btn success" type="submit">ยืนยัน</button>
+          <button class="btn primary" type="button" @click="$root.showModal = false">ยกเลิก</button>
+        </footer>
+      </form>
+    </modal>
+
+    <div class="collection-list column" v-if="accounts.length">
+      <div v-for="(account, index) in accounts">
+        <div class="collection-title">{{ account.provider }}</div>
+        <div class>{{ account.method == 'account' ? 'บัญชีธนาคาร' : 'PromptPay' }}</div>
+        <div class>{{ account.ref }}</div>
+
+        <div class="collection-action-bar">
+          <button class="btn-flat primary fas fa-trash-alt" @click="remove(account.id, index)"></button>
         </div>
       </div>
-      <div class="right-column w-col w-col-6">
-        <div class="box">
-          <h3 class="section-heading">เพิ่มช่องทางชำระเงิน</h3>
-          <div class="w-form">
-            <form id="email-form" name="email-form" data-name="Email Form">
-              <div class="form-group"><label for="name-2" class="form-label">Label</label><input type="text" id="name-2" name="name-2" data-name="Name 2" maxlength="256" class="form-input w-input"></div>
-              <div class="form-group"><label for="name-2" class="form-label">Label</label><input type="text" id="name-2" name="name-2" data-name="Name 2" maxlength="256" class="form-input w-input"></div>
-              <div class="form-group"><label for="name-2" class="form-label">Label</label><input type="text" id="name-2" name="name-2" data-name="Name 2" maxlength="256" class="form-input w-input"></div>
-              <div class="text-right"><a href="#" class="button submit w-button">ยืนยัน</a></div>
-            </form>
-            <div class="w-form-done">
-              <div>Thank you! Your submission has been received!</div>
-            </div>
-            <div class="w-form-fail">
-              <div>Oops! Something went wrong while submitting the form.</div>
-            </div>
-          </div>
-        </div>
-      </div>
+    </div>
+    <div class="column text-center" v-else>
+      <h3 class="is-size-4">ไม่มีช่องทางการชำระเงิน</h3>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-}
+  data() {
+    return {
+      accounts: [],
+      form: {
+        ref: null,
+        method: null,
+        provider: null
+      },
+      formVisible: false,
+      providers: [
+        {
+          name: "ธนาคารกรุงเทพ",
+          code: "BBL"
+        },
+        {
+          name: "ธนาคารกสิกรไทย",
+          code: "KBANK"
+        },
+        {
+          name: "ธนาคารกรุงไทย",
+          code: "KTB"
+        },
+        {
+          name: "ธนาคารไทยพาณิชย์",
+          code: "SCB"
+        },
+        {
+          name: "ธนาคารทหารไทย",
+          code: "TMB"
+        },
+        {
+          name: "ธนาคารออมสิน",
+          code: "GSB"
+        },
+        {
+          name: "ธนาคารกรุงศรีอยุธยา",
+          code: "BAY"
+        }
+      ]
+    };
+  },
+  methods: {
+    getPaymentMethod() {
+      this.$http.get("/get/payment_methods").then(response => {
+        this.accounts = response.data;
+      });
+    },
+    addPayment() {
+      this.$http
+        .post("/payments", {
+          ref: this.form.ref,
+          method: this.form.method,
+          provider: this.form.provider
+        })
+        .then(
+          response => {
+            this.accounts.push(response.data[0]);
+            toastr.success("เพิ่มช่องทางชำระเงินแล้ว");
+            this.form = {
+              ref: null,
+              method: null,
+              provider: null
+            };
+          },
+          response => {
+            toastr.error("เกิดข้อผิดพลาด");
+          }
+        );
+    },
+    remove(id, index) {
+      if (confirm("คุณแน่ใจหรือไม่ว่าจะลบ?")) {
+        this.$http.delete("/payments/" + id).then(
+          response => {
+            this.accounts.splice(index, 1);
+            toastr.success("ลบบัญชีเรียบร้อยแล้ว");
+          },
+          response => {
+            toastr.error("เกิดข้อผิดพลาด");
+          }
+        );
+      }
+    }
+  },
+  created() {
+    this.getPaymentMethod();
+  }
+};
 </script>
-
-<style lang="css">
-</style>
